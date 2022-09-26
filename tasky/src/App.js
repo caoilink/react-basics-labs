6fa4980142002b39c155a041f9de69feebfe4c7e
caoilink <caoilinkavanagh@gmail.com>
2022-09-26 10:27:05 +0100

    Completed Week 2 Lab B

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
+
+  const [taskState, setTaskState] = useState({
+    tasks: [
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 2, title: "Clean the shower", description: "Deep clean", deadline: "Tomorrow" },
+      { id: 2, title: "Hoover Carpet", description: "Use vanish", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      {taskState.tasks.map((task) => (
+        <Task
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task.id}
+        />
+      ))}
     </div>
   );
 }
