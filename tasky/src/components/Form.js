261250ace63e3fe5d7f10a82aaa53d36ec1f4be8
caoilink <caoilinkavanagh@gmail.com>
2022-09-26 11:48:41 +0100

    Week 3 Lab B done

--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -1,9 +1,66 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const AddTaskForm = (props) => {
 
   return (
+    <Box
+    component="form"
+    sx={{
+      '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+    }}
+    onSubmit={props.submit}
+  >
     <div>
+      <TextField
+        required
+        id="outlined-required"
+        name="title"
+        label="Task Title"
+        InputLabelProps={{ shrink: true }}
+        onChange={(event) => props.change(event)}
+      />
+    </div>
+    <div>
+      <TextField
+        required
+        name="deadline"
+        label="Deadline"
+        InputLabelProps={{ shrink: true }}
+        type="date"
+        onChange={(event) => props.change(event)}
+      />
+    </div>
+    <div>
+      <TextField
+        name="description"
+        id="outlined-multiline-static"
+        label="Task Details"
+        InputLabelProps={{ shrink: true }}
+        multiline
+        rows={4}
+        onChange={(event) => props.change(event)}
+      />
+    </div>
+    <div>
+      <Button 
+        type="submit"
+        variant="contained"
+        color="primary"
+        sx={{
+          m: 1,
+          p: 1,
+          width: '95%'
+        }}
+      >
+        Add Task
+      </Button>
+    </div>
+  </Box>
+
+   /* <div>
       <form onSubmit={props.submit}>
       <label>
         Task title:
@@ -22,6 +79,7 @@ const AddTaskForm = (props) => {
         <input type="submit" value="Submit" />
         </form>
     </div>
+  */
   )
 };
 
